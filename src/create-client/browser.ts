6ceab028c0b3f871fb38b7c07e3241fc4e98b0cc
Rafael Pizzo <rafaelpizzo@live.com>
2021-07-25 20:40:07 +0900

    remove unused dependencies

--- src/create-client/browser.ts
+++ src/create-client/browser.ts
@@ -1,22 +1,16 @@
-import i18n from 'i18next'
-import i18nextHTTPBackend from 'i18next-http-backend/cjs'
-import I18nextBrowserLanguageDetector from 'i18next-browser-languagedetector'
+import i18n from 'i18next';
+import i18nextHTTPBackend from 'i18next-http-backend/cjs';
 
-import { InitPromise } from '../../types'
+import { InitPromise } from '../../types';
 
 export default (config) => {
-  let initPromise: InitPromise
+    let initPromise: InitPromise;
 
-  if (!i18n.isInitialized) {
-    i18n.use(i18nextHTTPBackend)
-    if (config.browserLanguageDetection) {
-      const browserDetectors = new I18nextBrowserLanguageDetector()
-      config.customDetectors.forEach(detector => browserDetectors.addDetector(detector))
-      i18n.use(browserDetectors)
+    if (!i18n.isInitialized) {
+        i18n.use(i18nextHTTPBackend);
+
+        config.use.forEach((x) => i18n.use(x));
+        initPromise = i18n.init(config);
     }
-
-    config.use.forEach(x => i18n.use(x))
-    initPromise = i18n.init(config)
-  }
-  return { i18n, initPromise }
-}
+    return { i18n, initPromise };
+};
